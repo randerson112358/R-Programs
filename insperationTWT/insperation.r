d46f1925940b85ad18d30573f17d77a161ac6850
randerson112358 <randerson112358@users.noreply.github.com>
2018-03-21 23:22:59 -0400

    Update insperation.r

--- insperationTWT/insperation.r
+++ insperationTWT/insperation.r
@@ -1,3 +1,12 @@
+#This program takes insperational quotes from the text file 'insperation.txt' and posts it to twitter, depending on what week it is.
+#Example: if the text file contained the following sentences in this order and the current week of the year was week 2, then the 
+#program will post "have a good day!" to twitter:
+
+#Psuedo List:
+#"Be the best you can be !" - quote1
+#"have a good day!" - quote2
+#"You miss all the shots you never take" - quote3
+
 #Install the appropriate packages
 #install.packages("twitteR")
 library("twitteR")
@@ -25,9 +34,9 @@ setup_twitter_oauth(consumerKey,consumerSecret,accessToken,accessTokenSecret)
 FactsFile = read.table("C:/Tweets/insperationTWT/insperation.txt", sep='\n')
 
 
+#Gets the week number for the current year
 week= strftime(Sys.Date(), format="%V")
 weekNumber = as.numeric(week)
 
 #Post Tweets
-#tweet("A tweet")
 tweet(as.vector(FactsFile[weekNumber,1]))
